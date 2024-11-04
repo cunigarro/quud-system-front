b84fb5ad725f41082b6119ca0ea64929904ebcb3
Christian Unigarro <cunigarrod@gmail.com>
2024-11-04 11:40:04 -0500

    Add routes to login and register.

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
+  standalone: true,
+  imports: [RouterModule],
 })
 export class LandingComponent implements OnInit {
   constructor() {}
