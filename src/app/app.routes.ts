631c722bb5dd4bca7a079c43e6db74096bbe5ebd
Christian Unigarro <cunigarrod@gmail.com>
2024-11-04 11:35:12 -0500

    Create landing and pages for authentication.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'iniciar-sesion',
+    component: LoginComponent
+  },
+  {
+    path: 'registro',
+    component: RegisterComponent
+  },
+  {
+    path: 'admin',
+    component: DashboardComponent
+  }
+];
