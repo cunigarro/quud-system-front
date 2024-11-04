5211f9f2eb673681f64c637a1d7d0ff038f9899a
Christian Unigarro <cunigarrod@gmail.com>
2024-11-04 11:44:11 -0500

    Add links to login and register.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  standalone: true,
+  imports: [RouterModule],
 })
 export class LoginComponent implements OnInit {
   constructor() {}
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
+  standalone: true,
+  imports: [RouterModule],
 })
 export class RegisterComponent implements OnInit {
   constructor() {}
