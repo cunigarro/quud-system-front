3213c0b64d766c85c7339a44bf6ea3d795bf8363
Christian Unigarro <cunigarrod@gmail.com>
2025-02-16 16:49:59 -0500

    Stepper added with animation module in the root.

--- src/app/pages/dashboard/pages/register-repos/register-repos.component.ts
+++ src/app/pages/dashboard/pages/register-repos/register-repos.component.ts
@@ -1,11 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   templateUrl: './register-repos.component.html',
-  standalone: true
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule
+  ],
+  animations: [
+    trigger('horizontalStepTransition', [ // Define the animation here!
+      transition(':increment', [ // Transition when moving to the next step
+        style({ transform: 'translateX(100%)' }),
+        animate('500ms ease-in', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':decrement', [ // Transition when moving to the previous step
+        style({ transform: 'translateX(-100%)' }),
+        animate('500ms ease-in', style({ transform: 'translateX(0)' })),
+      ]),
+    ]),
+  ],
 })
-export class RegisterReposComponent implements OnInit {
-  constructor() { }
+export class RegisterReposComponent {
+  private _formBuilder = inject(FormBuilder);
 
-  ngOnInit(): void { }
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  isLinear = false;
 }
