0342ce14b650f80491f0d6ce5bb9883c92e7b34b
Christian Unigarro <cunigarrod@gmail.com>
2025-02-16 18:07:13 -0500

    Better styles for register repository form.

--- src/app/pages/dashboard/pages/register-repos/register-repos.component.ts
+++ src/app/pages/dashboard/pages/register-repos/register-repos.component.ts
@@ -1,12 +1,17 @@
-import {Component, inject} from '@angular/core';
-import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatButtonModule} from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon'
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import { animate, style, transition, trigger } from '@angular/animations';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   templateUrl: './register-repos.component.html',
@@ -20,26 +25,18 @@ import { animate, style, transition, trigger } from '@angular/animations';
     MatFormFieldModule,
     MatInputModule,
     CommonModule,
-    MatIconModule
-  ],
-  animations: [
-    trigger('horizontalStepTransition', [ // Define the animation here!
-      transition(':increment', [ // Transition when moving to the next step
-        style({ transform: 'translateX(100%)' }),
-        animate('500ms ease-in', style({ transform: 'translateX(0)' })),
-      ]),
-      transition(':decrement', [ // Transition when moving to the previous step
-        style({ transform: 'translateX(-100%)' }),
-        animate('500ms ease-in', style({ transform: 'translateX(0)' })),
-      ]),
-    ]),
+    MatIconModule,
+    MatSelectModule,
   ],
 })
 export class RegisterReposComponent {
   private _formBuilder = inject(FormBuilder);
 
   firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
+    projectName: ['', Validators.required],
+    urlRepository: ['', Validators.required],
+    language: ['', Validators.required],
+    languageVersion: ['', Validators.required],
   });
 
   secondFormGroup = this._formBuilder.group({
