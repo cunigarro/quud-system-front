2d582611ab59aefcb53582e8da468d07b2fb5211
Christian Unigarro <cunigarrod@gmail.com>
2025-02-16 19:50:12 -0500

    Add log out button

--- src/app/pages/dashboard/components/header/header.component.ts
+++ src/app/pages/dashboard/components/header/header.component.ts
@@ -2,20 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { mdiMenuDown } from '@mdi/js';
 import { SHARED_IMPORTS } from '../../../../shared/shared.imports';
 import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'dashboard-header',
   templateUrl: './header.component.html',
   standalone: true,
-  imports: [
-    SHARED_IMPORTS,
-    RouterModule
-  ]
+  imports: [SHARED_IMPORTS, RouterModule, MatMenuModule, RouterModule],
 })
 export class HeaderComponent implements OnInit {
   mdiMenuDown: string = mdiMenuDown;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
