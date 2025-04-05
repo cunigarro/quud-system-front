ab34bfe2b7a24b6ff193e458d8a7af29c193f6c7
Christian Unigarro <cunigarrod@gmail.com>
2025-04-05 16:58:02 -0500

    Improve rules groups view.

--- src/app/pages/dashboard/pages/rules-groups/rules-groups.component.ts
+++ src/app/pages/dashboard/pages/rules-groups/rules-groups.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   templateUrl: './rules-groups.component.html',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
 })
 export class RulesGroupsComponent implements OnInit {
   constructor() {}
