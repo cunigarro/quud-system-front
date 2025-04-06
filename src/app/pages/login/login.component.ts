226e30d547275f4d0c2d10f5973d705f56186e41
Christian Unigarro <cunigarrod@gmail.com>
2025-04-05 21:52:50 -0500

    Add login request.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule
+  ],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
+    private fb: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
 
-  login(evt: Event) {
-    evt.preventDefault();
-    this.router.navigate(['/admin']);
+  login(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.authService.login(this.loginForm.value)
+      .subscribe(() => {
+        this.router.navigate(['/admin']);
+      });
   }
 }
